3ac7001608cfbb7b9b63cf1fe3a3ce07f59fe4e2
agurimon <agurimon18@gmail.com>
2020-04-18 00:38:46 +0900

    parsing ethernet, arp packet  / trans mac-string -> bytes

--- packet.py
+++ packet.py
@@ -7,8 +7,15 @@ from binascii import unhexlify
 from ipaddress import IPv4Address
 
 
-def trans_bytes_mac(mac_address=None):
-    return unhexlify(mac_address.replace(':', ''))
+def trans_bytes_mac(mac_address='01:23:34:45:56:67'):
+    if len(mac_address) == 17 and type(mac_address) == str:
+        return unhexlify(mac_address.replace(':', ''))
+
+    elif len(mac_address) == 6 and type(mac_address) == bytes:
+        result_mac_address: str = ''
+        for byte_of_address in mac_address:
+            result_mac_address += '{:02x}'.format(byte_of_address) + ':'
+        return result_mac_address[:-1].lower()
 
 
 def trans_bytes_ip(ip):
@@ -28,30 +35,51 @@ class EthernetPacket:
         
         return destination_mac + source_mac + network_type
 
+    
+    def parse_packet(self, packet: bytes):
+        try:
+            assert not len(packet) != 14, \
+                'Bad packet length: ' + \
+                ' normal Ethernet header length: ' + '14'
+
+            ethernet_detailed = unpack('!' '6s' '6s' 'H', packet)
+
+            return {
+                'destination': trans_bytes_mac(ethernet_detailed[0]),
+                'source':      trans_bytes_mac(ethernet_detailed[1]),
+                'type':        int(ethernet_detailed[2])
+            }
+
+        except AssertionError as Error:
+            error_text = Error.args[0]
+
+        except IndexError:
+            error_text = 'Failed to parse Ethernet header!'
+
 
 class ArpPacket:
     eth = EthernetPacket()
     
     def __init__(self):
         self.destination_mac = 'ff:ff:ff:ff:ff:ff'
-        self.source_mac = None
-        self.sender_mac = None
-        self.sender_ip = None
-        self.target_mac = '00:00:00:00:00:00'
-        self.hw_type=1
-        self.protocol_type=2048
-        self.hw_length=6
-        self.protocol_length=4
-        self.opcode=1
-        self.network_type=2054
+        self.source_mac      = None
+        self.sender_mac      = None
+        self.sender_ip       = None
+        self.target_mac      = '00:00:00:00:00:00'
+        self.hw_type         = 1
+        self.protocol_type   = 2048
+        self.hw_length       = 6
+        self.protocol_length = 4
+        self.opcode          = 1
+        self.network_type    = 2054
 
 
     def save_data(self,
                   destination_mac='ff:ff:ff:ff:ff:ff',
-                  source_mac=     '12:34:56:78:9a:bc',
-                  sender_mac=     '12:34:56:78:9a:bc',
-                  sender_ip=      '192.168.0.1',
-                  target_mac=     '00:00:00:00:00:00'):
+                  source_mac     ='12:34:56:78:9a:bc',
+                  sender_mac     ='12:34:56:78:9a:bc',
+                  sender_ip      ='192.168.0.1',
+                  target_mac     ='00:00:00:00:00:00'):
         
         self.destination_mac = destination_mac
         self.source_mac = source_mac
@@ -63,8 +91,8 @@ class ArpPacket:
     def make_packet(self, target_ip='192.168.0.1'):
 
         eth_header = self.eth.make_header(destination_mac=self.destination_mac,
-                                          source_mac=self.source_mac,
-                                          network_type=self.network_type)
+                                          source_mac     =self.source_mac,
+                                          network_type   =self.network_type)
         arp_packet = b''
         arp_packet += pack('!H', self.hw_type)
         arp_packet += pack('!H', self.protocol_type)
@@ -86,3 +114,31 @@ class ArpPacket:
             arp_requests.append(self.make_packet(ip))
         
         return arp_requests
+
+    
+    def parse_packet(self, packet: bytes):
+        error_text: str = 'Failed to parse ARP packet!'
+        try:
+            assert not len(packet) != 28, \
+                ' Bad packet length: ' + 'self.base.error_text(str(len(packet)))' + \
+                ' normal ARP packet length: ' + 'self.base.success_text(str(RawARP.packet_length))'
+
+            arp_detailed = unpack('!' '2H' '2B' 'H' '6s' '4s' '6s' '4s', packet)
+
+            return {
+                'hardware-type': int(arp_detailed[0]),
+                'protocol-type': int(arp_detailed[1]),
+                'hardware-size': int(arp_detailed[2]),
+                'protocol-size': int(arp_detailed[3]),
+                'opcode':        int(arp_detailed[4]),
+                'sender-mac':    trans_bytes_mac(mac_address=arp_detailed[5]),
+                'sender-ip':     inet_ntoa(arp_detailed[6]),
+                'target-mac':    trans_bytes_mac(mac_address=arp_detailed[7]),
+                'target-ip':     inet_ntoa(arp_detailed[8])
+            }
+
+        except AssertionError as Error:
+            error_text += Error.args[0]
+
+        except IndexError:
+            pass
